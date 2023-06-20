29f72af6d48303ca64b2b071fd8dd3668072999b
Tanvir Rahman Khan <aeontanvir@gmail.com>
2023-06-20 14:20:08 -0500

    Clean

--- src/main/java/com/waa/backend/dtos/PropertyFilterDto.java
+++ src/main/java/com/waa/backend/dtos/PropertyFilterDto.java
@@ -1,12 +1,14 @@
 package com.waa.backend.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class PropertyFilterDto {
 
     private Long maxPrice;
--- src/main/java/com/waa/backend/services/ProjectService.java
+++
@@ -1,6 +0,0 @@
-package com.waa.backend.services;
-
-import com.waa.backend.dtos.ProjectDto;
-
-public interface ProjectService extends GenericCrudService<ProjectDto, ProjectDto, Long> {
-}
