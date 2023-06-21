728f46012fd3c3d59575cbab108e89be1997bafb
Md Rana Hossain <ranasl62@gmail.com>
2023-06-21 03:09:06 -0500

    git message controller fix

--- src/main/java/com/waa/backend/controllers/MessageController.java
+++ src/main/java/com/waa/backend/controllers/MessageController.java
@@ -1,7 +1,6 @@
 package com.waa.backend.controllers;
 
 import com.waa.backend.apiresponse.ApiResponse;
-import com.waa.backend.domains.User;
 import com.waa.backend.dtos.MessageDto;
 import com.waa.backend.dtos.PropertyDto;
 import com.waa.backend.services.MessageService;
@@ -33,7 +32,7 @@ public class MessageController {
 
     @GetMapping
     ResponseEntity<ApiResponse<List<MessageDto>>> findAll() {
-        return getMessagesForUser(Optional.empty());
+        return getMessagesForUser();
     }
 
     @GetMapping("/{id}")
@@ -44,22 +43,21 @@ public class MessageController {
         return ResponseEntity.ok(ApiResponse.error("Message Not Found"));
     }
 
-    @GetMapping("/user/{id}")
-    ResponseEntity<ApiResponse<List<MessageDto>>> getMessagesForUser(@PathVariable Optional<Long> userId) {
-        User user = getUser(userId);
-        return ResponseEntity.ok(ApiResponse.success("Message retrieved successfully.", messageService.getMessagesForUserOrderByDateTimeDesc(user)));
+    @GetMapping("/user")
+    ResponseEntity<ApiResponse<List<MessageDto>>> getMessagesForUser() {
+        return ResponseEntity.ok(ApiResponse.success("Message retrieved successfully.", messageService.getMessagesForUserOrderByDateTimeDesc(AUTH.getUserDetails())));
     }
 
     @GetMapping("/user/{userId}/property/{propertyId}")
-    ResponseEntity<ApiResponse<List<MessageDto>>> findAllByUserProperty(@PathVariable Optional<Long> userId, @PathVariable Optional<Long> propertyId) {
+    ResponseEntity<ApiResponse<List<MessageDto>>> findAllByUserProperty(@PathVariable Optional<Long> propertyId) {
         Long paramPropertyId = propertyId.orElse(null);
         PropertyDto propertyDto = null;
         if (paramPropertyId != null) {
             propertyDto = propertyService.getById(paramPropertyId);
         } else {
-            return getMessagesForUser(userId);
+            return getMessagesForUser();
         }
-        return ResponseEntity.ok(ApiResponse.success("Message retrieved successfully.", messageService.getMessagesForUserForPropertyOrderByDateTimeDesc(getUser(userId), propertyDto)));
+        return ResponseEntity.ok(ApiResponse.success("Message retrieved successfully.", messageService.getMessagesForUserForPropertyOrderByDateTimeDesc(AUTH.getUserDetails(), propertyDto)));
 
     }
 
@@ -67,23 +65,5 @@ public class MessageController {
     ResponseEntity<ApiResponse<MessageDto>> addMessage(@RequestBody MessageDto messageDto) {
         return ResponseEntity.ok(ApiResponse.success("Message retrieved successfully.", messageService.postMessage(messageDto, AUTH.getUserDetails())));
 
-    }
-
-    private User getUser(Optional<Long> userId) {
-        Long paramUserId = userId.orElse(null);
-        User user = AUTH.getUserDetails();
-        if (paramUserId !=
-                null) {
-            //Only admins can read all messages
-            if (user.getAuthorities().stream().filter(x -> x.getAuthority().equals("ADMIN")).toList().size() == 0) {
-                paramUserId = user.getId();
-            } else {
-                user = modelMapper.map(userService.getById(paramUserId), User.class);
-            }
-        } else {
-            paramUserId = user.getId();
-            user = modelMapper.map(userService.getById(paramUserId), User.class);
-        }
-        return user;
     }
 }
