d5e53528c3b3e87e3e90c0d56967ed23dfdb61bd
Md Rana Hossain <ranasl62@gmail.com>
2023-06-21 03:02:56 -0500

    git message controller fix

--- src/main/java/com/waa/backend/controllers/MessageController.java
+++ src/main/java/com/waa/backend/controllers/MessageController.java
@@ -11,14 +11,13 @@ import com.waa.backend.util.AUTH;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/messages")
 public class MessageController {
     @Autowired
